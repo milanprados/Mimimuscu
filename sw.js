9ddd8d30d1f2fb225048b0d584e8a2130f237ae2
milanprados <114026299+milanprados@users.noreply.github.com>
2026-08-11 04:12:58 +0200

    v32.2

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
- * Cache PWA V32.1
+ * Cache PWA V32.2
  * Navigation : network-first.
  * JS/CSS/data/images : cache-first pour un démarrage rapide et un offline fiable.
  */
-const VERSION = "mimi-muscu-v32-1";
+const VERSION = "mimi-muscu-v32-2";
 const APP_CACHE = `${VERSION}-app`;
 
 const APP_SHELL = [
